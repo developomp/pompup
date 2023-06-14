2a6fbdd89dc8d35c7ce96a6267239a28ffe2aa81
developomp <developomp@gmail.com>
2023-06-14 20:15:39 +0900

    feat: implement root command logic

--- cmd/root.go
+++ cmd/root.go
@@ -2,7 +2,13 @@ package cmd
 
 import (
 	"log"
+	"os"
 
+	"github.com/developomp/pompup/internal/check"
+	"github.com/developomp/pompup/internal/constants"
+	"github.com/developomp/pompup/internal/install"
+	"github.com/developomp/pompup/internal/ui"
+	"github.com/developomp/pompup/internal/workflows"
 	"github.com/spf13/cobra"
 )
 
@@ -22,5 +28,57 @@ var rootCmd = &cobra.Command{
 GitHub: https://github.com/developomp/pompup`,
 
 	Run: func(cmd *cobra.Command, args []string) {
+		initialize()
+		defer cleanup()
+
+		var postSetups []func()
+
+		// run setup functions
+		for i, selected := range ui.Select() {
+			if !selected {
+				continue
+			}
+
+			workflow := workflows.Workflows[i]
+			postSetups = append(postSetups, workflow.PostSetup)
+			workflow.Setup()
+		}
+
+		// run post-setup functions
+		for _, postSetup := range postSetups {
+			if postSetup == nil {
+				continue
+			}
+			postSetup()
+		}
+
 	},
+}
+
+func initialize() {
+	log.Println("Initializing...")
+	defer log.Println("Initialized!")
+
+	// perform startup checks
+	if err := check.StartupCheck(); err != nil {
+		log.Fatalln("Failed to start:", err)
+	}
+
+	// install dependencies
+	install.Deps()
+
+	// create temporary directory
+	if err := os.MkdirAll(constants.TmpDir, os.ModePerm); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func cleanup() {
+	log.Println("Wrapping up...")
+	defer log.Println("Done!")
+
+	// remove temporary directory
+	if err := os.RemoveAll(constants.TmpDir); err != nil {
+		log.Fatalln(err)
+	}
 }
