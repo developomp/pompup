2815d0959b52e75e36947ec2503c09e26d1bb9d5
developomp <developomp@gmail.com>
2023-06-15 22:14:32 +0900

    feat(workflow): add polychromatic

---
+++ internal/workflows/polychromatic.go
@@ -0,0 +1,20 @@
+package workflows
+
+import (
+	"os"
+	"os/exec"
+
+	"github.com/developomp/pompup/internal/install"
+)
+
+func init() {
+	register(&Workflow{
+		Name: "Dconf Editor",
+		Desc: "GSettings editor for GNOME",
+		Setup: func() {
+			install.Paru("openrazer-meta")
+			exec.Command("sudo", "gpasswd", "-a", os.Getenv("USER"), "plugdev").Run()
+			install.Paru("polychromatic")
+		},
+	})
+}
