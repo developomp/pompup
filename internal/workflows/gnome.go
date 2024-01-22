e02085a6400c4aa5e9cb28a67d21e0b0e20d0474
developomp <developomp@protonmail.com>
2024-01-22 19:58:28 +0900

    refactor: improve GNOME setup

--- internal/workflows/gnome.go
+++ internal/workflows/gnome.go
@@ -9,18 +9,46 @@ import (
 )
 
 //go:embed assets/etc/pipewire/pipewire.conf.d/99-low-latency.conf
-var pipewireLowLatencyConfig string
+var _pipewireLowLatencyConfig string
+
+//go:embed assets/dconf/gnome-desktop.conf
+var _gnomeDesktopDconf string
+
+//go:embed assets/dconf/gnome-keybindings.conf
+var _gnomeKeybindings string
+
+//go:embed assets/dconf/gnome-mutter.conf
+var _gnomeMutterDconf string
+
+//go:embed assets/dconf/gnome-settings-daemon.conf
+var _gnomeSettingsDaemon string
+
+//go:embed assets/dconf/extension-user-themes.conf
+var _gnomeExtensionUserThemesDconf string
 
 func init() {
 	register(&Workflow{
 		Name: "GNOME",
-		Desc: "GNOME desktop environment",
+		Desc: "minimal, usable GNOME Desktop Environment (DE)",
 		Tags: []Tag{System},
 		Setup: func() {
+			// core
 			install.Paru("gdm")
 			install.Paru("gnome-keyring")
+			install.Paru("gnome-control-center") // installs gnome-settings-daemon as dependency
+			install.Paru("xdg-desktop-portal-gnome")
+
+			// themes
+			install.Paru("pop-gtk-theme")      // gtk theme
+			install.Paru("papirus-icon-theme") // icon theme
+			install.Paru("xcursor-breeze")     // cursor theme
 
 			configurePipewire()
+			install.Dconf(_gnomeDesktopDconf)
+			install.Dconf(_gnomeKeybindings)
+			install.Dconf(_gnomeMutterDconf)
+			install.Dconf(_gnomeSettingsDaemon)
+			install.Dconf(_gnomeExtensionUserThemesDconf)
 		},
 	})
 }
@@ -31,7 +59,7 @@ func init() {
 // more info: https://gitlab.freedesktop.org/pipewire/pipewire/-/wikis/Config-PipeWire
 // more info about split conf: https://gitlab.freedesktop.org/pipewire/pipewire/-/wikis/Config-PipeWire#split-file-configuration
 func configurePipewire() {
-	err := helper.SudoWriteFile("/etc/pipewire/pipewire.conf.d/99-low-latency.conf", pipewireLowLatencyConfig)
+	err := helper.SudoWriteFile("/etc/pipewire/pipewire.conf.d/99-low-latency.conf", _pipewireLowLatencyConfig)
 	if err != nil {
 		pterm.Fatal.Println("Failed to configure pipewire")
 	}
