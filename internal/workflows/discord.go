d4bc2fa029e42b012887e28945f79d7b4d164e2f
developomp <developomp@protonmail.com>
2024-01-29 19:30:23 +0900

    refactor: replace BetterDiscord -> Vencord

--- internal/workflows/discord.go
+++ internal/workflows/discord.go
@@ -1,21 +1,8 @@
 package workflows
 
 import (
-	"embed"
-	"fmt"
-	"io"
-	"io/fs"
-
-	"github.com/developomp/pompup/internal/helper"
 	"github.com/developomp/pompup/internal/install"
-	"github.com/pterm/pterm"
 )
-
-//go:embed assets/betterdiscord/plugins
-var _BDPlugins embed.FS
-
-// BDPlugins is a collection of BetterDiscord plugin and configuration files
-var BDPlugins, _ = fs.Sub(_BDPlugins, "assets/betterdiscord/plugins")
 
 func init() {
 	register(&Workflow{
@@ -24,59 +11,9 @@ func init() {
 		Tags: []Tag{Gui},
 		Setup: func() {
 			install.Flatpak("com.discordapp.Discord")
-
-			// BetterDiscord stuff
-			install.Paru("betterdiscordctl-git")                       // BetterDiscord installer
-			helper.Run("betterdiscordctl", "-i", "flatpak", "install") // install BetterDiscord
-			installBDPlugins()                                         // install Plugins
 		},
 		Reminders: []string{
-			"Enable BetterDiscord plugins in discord settings",
+			"Install vencord",
 		},
 	})
-}
-
-func installBDPlugins() {
-	pterm.Debug.Printfln("Installing BetterDiscord plugins")
-
-	// go through each BetterDiscord plugin files
-	err := fs.WalkDir(BDPlugins, ".", func(pluginName string, entry fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		// skip if not file
-		if entry.IsDir() {
-			return nil
-		}
-
-		pluginPath := fmt.Sprintf(
-			"%s/.var/app/com.discordapp.Discord/config/BetterDiscord/plugins/%s",
-			helper.GetHomeDir(),
-			pluginName,
-		)
-
-		file, err := BDPlugins.Open(pluginName)
-		if err != nil {
-			return err
-		}
-
-		data, err := io.ReadAll(file)
-		if err != nil {
-			return err
-		}
-
-		err = helper.WriteFile(pluginPath, data)
-		if err != nil {
-			return err
-		}
-
-		pterm.Debug.Printfln("created %s", pluginPath)
-
-		return nil
-	})
-
-	if err != nil {
-		pterm.Fatal.Println("Failed to install BetterDiscord plugin:", err)
-	}
 }
