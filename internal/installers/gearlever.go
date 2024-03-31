c9eba96ba34085797cbbf89054a9b0b9376b9664
developomp <developomp@protonmail.com>
2024-03-31 20:26:37 +0900

    feat: improve gearlever installer

--- internal/installers/gearlever.go
+++ internal/installers/gearlever.go
@@ -1,12 +1,28 @@
 package installers
 
-import "github.com/developomp/pompup/internal/wrapper"
+import (
+	"fmt"
+
+	"github.com/developomp/pompup/internal/wrapper"
+)
 
 var gearleverInstaller = Installer{
 	Name: "GearLever",
 	Desc: "appimage manager",
 	Tags: []Tag{System, Gui},
 	Setup: func() {
+		if wrapper.IsBinInstalled("it.mijorus.gearlever") {
+			return
+		}
+
+		// set default appimage location to "~/Programs/AppImages"
+		wrapper.Run(
+			"sed",
+			"-i",
+			fmt.Sprintf("/^appimages-default-folder=/c\\appimages-default-folder='%s/Programs/AppImages'", wrapper.GetHomeDir()),
+			"/home/pomp/.var/app/it.mijorus.gearlever/config/glib-2.0/settings/keyfile",
+		)
+
 		wrapper.Flatpak("it.mijorus.gearlever")
 	},
 }
