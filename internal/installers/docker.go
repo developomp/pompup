3e78223ce33d9e2a43a95d9ba719181f493ee25c
developomp <developomp@protonmail.com>
2024-04-02 21:18:21 +0900

    feat: improve docker installer skip logic

--- internal/installers/docker.go
+++ internal/installers/docker.go
@@ -1,6 +1,9 @@
 package installers
 
 import (
+	"os/exec"
+	"strings"
+
 	"github.com/developomp/pompup/internal/wrapper"
 	"github.com/pterm/pterm"
 )
@@ -11,23 +14,28 @@ func init() {
 		Desc: "not a VM",
 		Tags: []Tag{Cli, Dev},
 		Setup: func() {
-			if wrapper.IsBinInstalled("docker") {
-				return
+			wrapper.ParuOnce("docker")
+
+			out, err := exec.Command("groups").Output()
+			if err != nil {
+				pterm.Fatal.Println("Failed to get group names:", err)
+			}
+			if !strings.Contains(string(out), "docker") {
+				// add current user to the "docker" group
+				// this allows us to use docker commands without sudo
+				username := wrapper.GetUserName() // assumes there's no space in the username
+				err = wrapper.Run("sudo", "usermod", "-aG", "docker", username)
+				if err != nil {
+					pterm.Fatal.Println("Failed to add", username, "to docker group:", err)
+				}
 			}
 
-			wrapper.Paru("docker")
-
-			// add current user to the "docker" group
-			// this allows us to use docker commands without sudo
-			username := wrapper.GetUserName() // assumes there's no space in the username
-			err := wrapper.Run("sudo", "usermod", "-aG", "docker", username)
+			err = wrapper.Run("systemctl", "is-active", "docker")
 			if err != nil {
-				pterm.Fatal.Println("Failed to add", username, "to docker group:", err)
-			}
-
-			err = wrapper.Run("sudo", "systemctl", "--now", "enable", "docker")
-			if err != nil {
-				pterm.Fatal.Println("Failed to start docker service:", err)
+				err = wrapper.Run("sudo", "systemctl", "--now", "enable", "docker")
+				if err != nil {
+					pterm.Fatal.Println("Failed to start docker service:", err)
+				}
 			}
 		},
 	})
