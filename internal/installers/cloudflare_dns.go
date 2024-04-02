b80a2fdd806d4ac98ba62afeb8f21a85d0f40fb2
developomp <developomp@protonmail.com>
2024-04-02 21:05:27 +0900

    feat: improve cloudflare DNS installer skip logic

--- internal/installers/cloudflare_dns.go
+++ internal/installers/cloudflare_dns.go
@@ -2,6 +2,8 @@ package installers
 
 import (
 	_ "embed"
+	"os"
+	"strings"
 
 	"github.com/developomp/pompup/internal/wrapper"
 	"github.com/pterm/pterm"
@@ -16,20 +18,30 @@ func init() {
 		Desc: "1.1.1.1 DNS",
 		Tags: []Tag{System},
 		Setup: func() {
+			const filePath = "/etc/resolv.conf"
+
+			b, err := os.ReadFile(filePath)
+			if err != nil {
+				pterm.Fatal.Printfln("Failed to read %s: %s", filePath, err)
+			}
+			if strings.TrimSpace(string(b)) == _resolvConf {
+				return
+			}
+
 			// remove immutable flag from /etc/resolv.conf
-			err := wrapper.Run("sudo", "chattr", "-i", "/etc/resolv.conf")
+			err = wrapper.Run("sudo", "chattr", "-i", filePath)
 			if err != nil {
 				pterm.Fatal.Println("Failed to remove immutable flag from /etc/resolv.conf:", err)
 			}
 
 			// write to /etc/resolv.conf
-			err = wrapper.SudoWriteFile("/etc/resolv.conf", _resolvConf)
+			err = wrapper.SudoWriteFile(filePath, _resolvConf)
 			if err != nil {
 				pterm.Fatal.Println("Failed to write /etc/resolv.conf:", err)
 			}
 
 			// restore immutable flag
-			err = wrapper.Run("sudo", "chattr", "+i", "/etc/resolv.conf")
+			err = wrapper.Run("sudo", "chattr", "+i", filePath)
 			if err != nil {
 				pterm.Fatal.Println("Failed to restore immutable flag of /etc/resolv.conf:", err)
 			}
