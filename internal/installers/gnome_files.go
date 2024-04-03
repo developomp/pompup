c3f19dbb54b7caae7d76ea9550749cea5144f2ff
developomp <developomp@protonmail.com>
2024-04-03 18:04:04 +0900

    feat: improve GNOME files installer skip logic

--- internal/installers/gnome_files.go
+++ internal/installers/gnome_files.go
@@ -15,14 +15,10 @@ func init() {
 		Desc: "nautilus",
 		Tags: []Tag{Gnome, Gui},
 		Setup: func() {
-			if wrapper.IsArchPkgInstalled("nautilus") {
-				return
-			}
-
-			wrapper.Paru("nautilus")
+			wrapper.ParuOnce("nautilus")
 			wrapper.TryDconf(_gnomeNautilusDconf)
 
-			wrapper.Paru("nautilus-open-any-terminal") // allow nautilus to open directory in terminal
+			wrapper.ParuOnce("nautilus-open-any-terminal") // allow nautilus to open directory in terminal
 			kittyInstaller.Setup()
 
 			wrapper.Run("gsettings", "set", "com.github.stunkymonkey.nautilus-open-any-terminal", "terminal", "kitty")
