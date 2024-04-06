51cdf64e0dc39debbe476893c5d8697f46a1db4c
developomp <developomp@protonmail.com>
2024-04-06 11:32:48 +0900

    fix: polychromatic installer not working

--- internal/installers/polychromatic.go
+++ internal/installers/polychromatic.go
@@ -4,13 +4,16 @@ import "github.com/developomp/pompup/internal/wrapper"
 
 func init() {
 	register(&Installer{
-		Name: "Dconf Editor",
-		Desc: "GSettings editor for GNOME",
+		Name: "Polychromatic",
+		Desc: "openrazer frontend",
 		Tags: []Tag{Configurator},
 		Setup: func() {
-			wrapper.ParuOnce("openrazer-meta")
+			// install openrazer
+			wrapper.ParuOnce("openrazer-daemon")
+			wrapper.ParuOnce("openrazer-driver-dkms")
 			wrapper.AddGroup("plugdev")
 
+			// install polychromatic
 			wrapper.ParuOnce("polychromatic")
 		},
 	})
