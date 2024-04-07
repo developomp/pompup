d58ae1fd7d3c59314f2405740b60b7dcf426e75a
developomp <developomp@protonmail.com>
2024-04-07 17:24:56 +0900

    fix: improve virtualbox install skip logic

--- internal/installers/virtualbox.go
+++ internal/installers/virtualbox.go
@@ -11,24 +11,14 @@ func init() {
 		Desc: "VM stuff",
 		Tags: []Tag{Dev, Gui},
 		Setup: func() {
-			if wrapper.IsArchPkgInstalled("virtualbox") {
-				return
-			}
+			wrapper.ParuOnce("virtualbox")
+			wrapper.ParuOnce("virtualbox-host-modules-arch")
+			wrapper.ParuOnce("virtualbox-ext-oracle")
 
-			wrapper.Paru("virtualbox")
-			wrapper.Paru("virtualbox-host-modules-arch")
-			wrapper.Paru("virtualbox-ext-oracle")
-
-			username := wrapper.GetUserName()
-
-			// allow usage of virtualbox without root perm
-			err := wrapper.Run("sudo", "gpasswd", "-a", username, "vboxusers")
-			if err != nil {
-				pterm.Fatal.Println("Failed to add", username, "to vboxusers group:", err)
-			}
+			wrapper.AddGroup("vboxusers") // allow usage of virtualbox without root perm
 
 			// load vboxdrv, vboxnetadp, and vboxnetflt drivers
-			err = wrapper.Run("sudo", "systemctl", "restart", "systemd-modules-load")
+			err := wrapper.Run("sudo", "systemctl", "restart", "systemd-modules-load")
 			if err != nil {
 				pterm.Fatal.Println("Failed to load virtualbox driver modules:", err)
 			}
