a2402353aa3e78e0e692f6e4329c198145e8bb45
developomp <developomp@protonmail.com>
2024-04-07 20:14:17 +0900

    feat: handle package conflict when installing rust

--- internal/installers/rust.go
+++ internal/installers/rust.go
@@ -2,6 +2,7 @@ package installers
 
 import (
 	"github.com/developomp/pompup/internal/wrapper"
+	"github.com/pterm/pterm"
 )
 
 func init() {
@@ -10,6 +11,14 @@ func init() {
 		Desc: "Rustup and stuff",
 		Tags: []Tag{Cli, Dev},
 		Setup: func() {
+			if wrapper.IsArchPkgInstalled("rust") {
+				// rust conflicts with rustup. Remove before installing rustup.
+
+				if err := wrapper.Run("paru", "-R", "--noconfirm", "rust"); err != nil {
+					pterm.Fatal.Println("Failed to remove rust package:", err)
+				}
+			}
+
 			wrapper.ParuOnce("rustup")
 
 			wrapper.Run("rustup", "install", "stable")
