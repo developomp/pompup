db9222397592f08dc4c7fed747778cc8347b3dac
developomp <developomp@protonmail.com>
2024-04-07 21:18:35 +0900

    feat: add tree installer

---
+++ internal/installers/tree.go
@@ -0,0 +1,14 @@
+package installers
+
+import "github.com/developomp/pompup/internal/wrapper"
+
+func init() {
+	register(&Installer{
+		Name: "Tree",
+		Desc: "Show directory structure",
+		Tags: []Tag{Cli},
+		Setup: func() {
+			wrapper.ParuOnce("tree")
+		},
+	})
+}
