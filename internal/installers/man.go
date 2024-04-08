1e3237cfe73f6d54bd3ebfcbdb5df9b7c65967b2
developomp <developomp@protonmail.com>
2024-04-08 18:29:30 +0900

    feat: add man DB installer

---
+++ internal/installers/man.go
@@ -0,0 +1,14 @@
+package installers
+
+import "github.com/developomp/pompup/internal/wrapper"
+
+func init() {
+	register(&Installer{
+		Name: "man DB",
+		Desc: "Arch Linux manual",
+		Tags: []Tag{System, Cli},
+		Setup: func() {
+			wrapper.ParuOnce("man-db")
+		},
+	})
+}
