7e83bef71351bddc3779dfd74e011790b3810780
developomp <developomp@protonmail.com>
2024-04-14 13:05:17 +0900

    feat: update discord installer to use vesktop

--- internal/installers/discord.go
+++ internal/installers/discord.go
@@ -1,16 +1,30 @@
 package installers
 
-import "github.com/developomp/pompup/internal/wrapper"
+import (
+	_ "embed"
+
+	"github.com/developomp/pompup/internal/wrapper"
+)
+
+//go:embed assets/home/.var/app/dev.vencord.Vesktop/config/vesktop/settings/settings.json
+var _vesktop_settings []byte
+
+//go:embed assets/home/.var/app/dev.vencord.Vesktop/config/vesktop/themes/nordic.theme.css
+var _vesktop_theme []byte
+
+//go:embed assets/home/.var/app/dev.vencord.Vesktop/config/vesktop/settings.json
+var _vesktop_launcher_settings []byte
 
 func init() {
 	register(&Installer{
 		Name: "Discord",
-		Desc: "discord and betterdiscord plugins",
+		Desc: "gamer chat platform",
 		Setup: func() {
-			wrapper.FlatpakOnce("com.discordapp.Discord")
-		},
-		Reminders: []string{
-			"Install vencord",
+			wrapper.FlatpakOnce("dev.vencord.Vesktop")
+
+			wrapper.WriteFile(wrapper.InHome(".var/app/dev.vencord.Vesktop/config/vesktop/settings/settings.json"), _vesktop_settings)
+			wrapper.WriteFile(wrapper.InHome(".var/app/dev.vencord.Vesktop/config/vesktop/nordic.theme.css"), _vesktop_theme)
+			wrapper.WriteFile(wrapper.InHome(".var/app/dev.vencord.Vesktop/config/vesktop/settings.json"), _vesktop_launcher_settings)
 		},
 	})
 }
