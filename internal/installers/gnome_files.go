94388c0ab196e280ae50009c5f03fd6008a8d213
developomp <developomp@protonmail.com>
2024-04-14 23:00:08 +0900

    feat: add nautilus bookmarks

--- internal/installers/gnome_files.go
+++ internal/installers/gnome_files.go
@@ -2,12 +2,16 @@ package installers
 
 import (
 	_ "embed"
+	"strings"
 
 	"github.com/developomp/pompup/internal/wrapper"
 )
 
 //go:embed assets/dconf/gnome-nautilus.conf
 var _gnomeNautilusDconf string
+
+//go:embed assets/home/.config/gtk-3.0/bookmarks
+var _gnomeNautilusBookmarks string
 
 func init() {
 	register(&Installer{
@@ -17,8 +21,11 @@ func init() {
 			wrapper.ParuOnce("nautilus")
 			wrapper.TryDconf(_gnomeNautilusDconf)
 
-			wrapper.ParuOnce("nautilus-open-any-terminal") // allow nautilus to open directory in terminal
+			_gnomeNautilusBookmarks = strings.ReplaceAll(_gnomeNautilusBookmarks, "$HOME", wrapper.GetHomeDir())
+			wrapper.WriteFile(wrapper.InHome(".config/gtk-3.0/bookmarks"), []byte(_gnomeNautilusBookmarks))
 
+			// allow nautilus to open directories in terminal
+			wrapper.ParuOnce("nautilus-open-any-terminal")
 			wrapper.Run("gsettings", "set", "com.github.stunkymonkey.nautilus-open-any-terminal", "terminal", "kitty")
 			wrapper.Run("gsettings", "set", "com.github.stunkymonkey.nautilus-open-any-terminal", "keybindings", "''")
 			wrapper.Run("gsettings", "set", "com.github.stunkymonkey.nautilus-open-any-terminal", "new-tab", "true")
