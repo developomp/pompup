4176b2b721130d77322f86162532ad2eea50ecef
developomp <developomp@protonmail.com>
2024-04-26 21:46:10 +0900

    fix: latexindent not working

--- internal/installers/texlive.go
+++ internal/installers/texlive.go
@@ -8,6 +8,12 @@ func init() {
 		Desc: "LaTeX document for true scholar",
 		Setup: func() {
 			wrapper.ParuOnce("texlive-meta")
+
+			// https://github.com/Glavin001/atom-beautify/issues/1792#issuecomment-327071117
+			wrapper.Run("sudo", "cpan", "Unicode::GCString")
+			wrapper.Run("sudo", "cpan", "App::cpanminus")
+			wrapper.Run("sudo", "cpan", "YAML::Tiny")
+			wrapper.Run("sudo", "perl", "-MCPAN", "-e", "install \"File::HomeDir\"")
 		},
 	})
 }
