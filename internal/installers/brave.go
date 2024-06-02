0df6225d6e5b5389853dcb0bca2ecb65b5043329
developomp <developomp@protonmail.com>
2024-06-02 12:03:59 +0900

    feat: customize brave browser desktop entry

--- internal/installers/brave.go
+++ internal/installers/brave.go
@@ -1,6 +1,14 @@
 package installers
 
-import "github.com/developomp/pompup/internal/wrapper"
+import (
+	_ "embed"
+
+	"github.com/developomp/pompup/internal/wrapper"
+	"github.com/pterm/pterm"
+)
+
+//go:embed assets/var/lib/flatpak/app/com.brave.Browser/current/active/files/share/applications/com.brave.Browser.desktop
+var _braveDesktopEntry string
 
 func init() {
 	register(&Installer{
@@ -8,6 +16,13 @@ func init() {
 		Desc: "Least worst browser",
 		Setup: func() {
 			wrapper.FlatpakOnce("com.brave.Browser")
+
+			const braveDesktopEntryPath = "/var/lib/flatpak/app/com.brave.Browser/current/active/files/share/applications/com.brave.Browser.desktop"
+			wrapper.SudoWriteFile(braveDesktopEntryPath, _braveDesktopEntry)
+			err := wrapper.Run("sudo", "chmod", "+x", braveDesktopEntryPath)
+			if err != nil {
+				pterm.Fatal.Printfln("Failed to make %s executable: %s", braveDesktopEntryPath, err)
+			}
 		},
 		Reminders: []string{
 			"Enable Brave sync",
