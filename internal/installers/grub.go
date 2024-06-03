fd8ce65f74df02bd47e4e882f1a9ebfaf476e3da
developomp <developomp@protonmail.com>
2024-06-03 14:59:19 +0900

    refactor: simplify grub installer

--- internal/installers/grub.go
+++ internal/installers/grub.go
@@ -1,11 +1,14 @@
 package installers
 
 import (
-	"fmt"
+	_ "embed"
 
 	"github.com/developomp/pompup/internal/wrapper"
 	"github.com/pterm/pterm"
 )
+
+//go:embed assets/etc/default/grub
+var _grubConfig string
 
 func init() {
 	register(&Installer{
@@ -14,27 +17,10 @@ func init() {
 		Setup: func() {
 			wrapper.ParuOnce("grub")
 
-			changed := false
-			const filePath = "/etc/default/grub"
+			const configPath = "/etc/default/grub"
+			if !wrapper.IsFileUpdated(configPath, _grubConfig) {
+				wrapper.SudoWriteFile(configPath, _grubConfig)
 
-			// The sed commands below are explained here: https://stackoverflow.com/a/11245501/12979111
-
-			if !isLineInFile("GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet splash nvidia-drm.modeset=1\"", filePath) {
-				changed = true
-				wrapper.Run("sudo", "sed", "-i", "/^GRUB_CMDLINE_LINUX_DEFAULT=/c\\GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet splash nvidia-drm.modeset=1\"", filePath)
-			}
-
-			if !isLineInFile("GRUB_TIMEOUT_STYLE=hidden", filePath) {
-				changed = true
-				wrapper.Run("sudo", "sed", "-i", "/^GRUB_TIMEOUT_STYLE=/c\\GRUB_TIMEOUT_STYLE=hidden", filePath)
-			}
-
-			if !isLineInFile("GRUB_TIMEOUT=0", filePath) {
-				changed = true
-				wrapper.Run("sudo", "sed", "-i", "/^GRUB_TIMEOUT=/c\\GRUB_TIMEOUT=0", filePath)
-			}
-
-			if changed {
 				err := wrapper.Run("sudo", "grub-mkconfig", "-o", "/boot/grub/grub.cfg")
 				if err != nil {
 					pterm.Fatal.Println("Failed to generate /boot/grub/grub.cfg:", err)
@@ -42,11 +28,4 @@ func init() {
 			}
 		},
 	})
-}
-
-func isLineInFile(line string, filePath string) bool {
-	// ^: beginning of line
-	// $: end of line
-	err := wrapper.Run("grep", fmt.Sprintf("^%s$", line), filePath)
-	return err == nil
 }
