765e21c8ef5a5603f60003619d23f3823b82eac0
developomp <developomp@protonmail.com>
2024-06-03 19:00:58 +0900

    feat: update vscode extensions

--- internal/installers/vscodium.go
+++ internal/installers/vscodium.go
@@ -40,146 +40,120 @@ func init() {
 
 			// vscodeExtensions is a list of vscode extensions retrieved by using the following command: codium --list-extensions
 			var vscodeExtensions = []string{
-				// General
-				"mikestead.dotenv",
-				"mkxml.vscode-filesize",
-				"WakaTime.vscode-wakatime",
+				// general vscde features
+				"aaron-bond.better-comments",            // comment color highlighting
+				"EditorConfig.EditorConfig",             // EditorConfig support
+				"mikestead.dotenv",                      // .env file support
+				"mkxml.vscode-filesize",                 // show file size on bottom bar
+				"streetsidesoftware.code-spell-checker", // spell checking
+				"WakaTime.vscode-wakatime",              // Wakatime support
+				"usernamehw.errorlens",                  // show error on lines they originated from
 
-				// Code Tests
-				"hbenl.vscode-test-explorer",
-				"ms-vscode.test-adapter-converter",
+				// general editing feature
+				"christian-kohler.path-intellisense",      // autocomplete filenames
+				"dbankier.vscode-quick-select",            // selecting quoted text
+				"earshinov.sort-lines-by-selection",       // sort lines by selection
+				"stkb.rewrap",                             // auto-wrap long text
+				"plievone.vscode-template-literal-editor", // template string syntax highlight
 
-				// Editor
-				"dbaeumer.vscode-eslint",
-				"christian-kohler.path-intellisense",
-				"usernamehw.errorlens",
-				"earshinov.sort-lines-by-selection",
-				"EditorConfig.EditorConfig",
-				"dbankier.vscode-quick-select",
-				"hajdaini.select-until-pattern",
-				"esbenp.prettier-vscode",
-				"aaron-bond.better-comments",
-				"streetsidesoftware.code-spell-checker",
-				"stkb.rewrap",
+				// general web tech
+				"bradlc.vscode-tailwindcss",     // Tailwind support
+				"dbaeumer.vscode-eslint",        // ESLint support
+				"esbenp.prettier-vscode",        // Prettier support
+				"formulahendry.auto-rename-tag", // HTML/XML tag auto rename
+				"jock.svg",                      // SVG support
+				"naumovs.color-highlight",       // Color stuff like #000000
+				"stylelint.vscode-stylelint",    // CSS linter
+
+				// JS & TS ecosystem
+				"oven.bun-vscode",                     // bun support
+				"denoland.vscode-deno",                // deno support
+				"yoavbls.pretty-ts-errors",            // make TS error human readable
+				"bierner.jsdoc-markdown-highlighting", // markdown syntax highlighting for jsdoc comments
+				"vunguyentuan.vscode-postcss",         // postcss support
 
 				// icons & themes
-				"PKief.material-icon-theme",
-				"zhuangtongfa.material-theme", // One Dark Pro
+				"PKief.material-icon-theme",   // explorer icon theme
+				"zhuangtongfa.material-theme", // One Dark Pro theme
 
-				// Git
-				"eamodio.gitlens",
-				"mhutchie.git-graph",
-				"vivaxy.vscode-conventional-commits",
+				// git / github
+				"eamodio.gitlens",              // basic git action as GUI
+				"github.vscode-github-actions", // Github action features
 
-				// GitHub
-				"github.vscode-github-actions",
-
-				// HTML, CSS, Design
-				"jock.svg",
-				"formulahendry.auto-rename-tag",
-				"bradlc.vscode-tailwindcss",
-				"naumovs.color-highlight",
-				"stylelint.vscode-stylelint",
-
-				// Reverse Engineering
-				"icsharpcode.ilspy-vscode",
-				"ms-vscode.hexeditor",
-				"tintinweb.vscode-decompiler",
+				// reverse engineering
+				"tintinweb.vscode-decompiler", // Decompile jar, apk, pyc, pyo, binary executable, etc
+				"icsharpcode.ilspy-vscode",    // C# reverse engineering
+				"ms-vscode.hexeditor",         // hex editor
 
 				// Markdown
-				"jeff-tian.markdown-katex",
-				"bierner.markdown-mermaid",
-				"bpruitt-goddard.mermaid-markdown-syntax-highlighting",
-				"yzhang.markdown-all-in-one",
-
-				// JS & TS Ecosystem
-				"oven.bun-vscode",
-				"denoland.vscode-deno",
-				"idered.npm",
-				"DigitalBrainstem.javascript-ejs-support",
-				"plievone.vscode-template-literal-editor",
-				"dsznajder.es7-react-js-snippets",
-				"yoavbls.pretty-ts-errors",
-				"bierner.jsdoc-markdown-highlighting",
-				"vunguyentuan.vscode-postcss",
+				"yzhang.markdown-all-in-one",                           // markdown swiss army knife
+				"jeff-tian.markdown-katex",                             // markdown KaTeX support
+				"bierner.markdown-mermaid",                             // markdown mermaid preview
+				"bpruitt-goddard.mermaid-markdown-syntax-highlighting", // markdown mermaid color highlight
 
 				// Rust
-				"tamasfe.even-better-toml",
-				"bungcip.better-toml",
-				"rust-lang.rust-analyzer",
-				"serayuzgur.crates",
+				"rust-lang.rust-analyzer",  // rust support
+				"tamasfe.even-better-toml", // toml support
+				"serayuzgur.crates",        // crates.io features (crate version, vscode palette actions, etc)
 
 				// Python
-				"donjayamanne.python-extension-pack",
-				"ms-python.black-formatter",
-				"ms-python.isort",
-				"ms-python.python",
-				"ms-python.vscode-pylance",
-				"ms-toolsai.jupyter-keymap",
-				"ms-toolsai.jupyter-renderers",
-				"ms-toolsai.jupyter",
-				"ms-toolsai.vscode-jupyter-cell-tags",
-				"ms-toolsai.vscode-jupyter-slideshow",
+				"charliermarsh.ruff",        // fast python formatter with drop-in parity with flake8, isort, and black
+				"ms-python.black-formatter", // popular python formatter
+				"ms-python.python",          // python support
+				"ms-toolsai.jupyter",        // jupyter extension pack
 
 				// Go
-				"golang.go",
+				"golang.go", // Go support
 
 				// C++
-				"ms-vscode.cpptools",
+				"ms-vscode.cpptools", // C/C++ support
 
 				// C#
-				"ms-dotnettools.csdevkit",
-				"ms-dotnettools.csharp",
-				"ms-dotnettools.vscode-dotnet-runtime",
-				"ms-vscode.mono-debug",
-
-				// Svelte
-				"svelte.svelte-vscode",
-				"ardenivanov.svelte-intellisense",
-
-				// Dart, Flutter
-				"dart-code.dart-code",
-				"dart-code.flutter",
-
-				// Shell
-				"foxundermoon.shell-format",
-				"timonwong.shellcheck",
-
-				// Docker
-				"ms-azuretools.vscode-docker",
-
-				// LaTeX
-				"James-Yu.latex-workshop",
-
-				// vim
-				"XadillaX.viml",
-
-				// CSV
-				"mechatroner.rainbow-csv",
-
-				// Godot
-				"alfish.godot-files",
-				"geequlim.godot-tools",
-				"neikeq.godot-csharp-vscode",
-
-				// nginx
-				"ahmadalli.vscode-nginx-conf",
-
-				// XML
-				"redhat.vscode-xml",
-
-				// Linux
-				"coolbear.systemd-unit-file",
-				"nico-castell.linux-desktop-file",
+				"ms-dotnettools.csharp",                // C# support
+				"ms-dotnettools.csdevkit",              // C# dev tools
+				"ms-dotnettools.vscode-dotnet-runtime", // .NET install tools
+				"ms-vscode.mono-debug",                 // mono Debugging
 
 				// Unity
-				"visualstudiotoolsforunity.vstuc",
+				"visualstudiotoolsforunity.vstuc", // unity-vscode integration
+
+				// Svelte
+				"svelte.svelte-vscode",            // svelte support
+				"ardenivanov.svelte-intellisense", // svelte intellisense
+
+				// Shell
+				"foxundermoon.shell-format", // shell formatting
+				"timonwong.shellcheck",      // shell linting
+
+				// Docker
+				"ms-azuretools.vscode-docker", // docker support
+
+				// LaTeX
+				"James-Yu.latex-workshop", // LaTeX support
+
+				// vim
+				"XadillaX.viml", // vim script support
+
+				// CSV
+				"mechatroner.rainbow-csv", // better csv coloring
+
+				// Godot
+				"alfish.godot-files",         // godot file support
+				"geequlim.godot-tools",       // godot-vscode integratrion
+				"neikeq.godot-csharp-vscode", // godot C# tools
+
+				// nginx
+				"ahmadalli.vscode-nginx-conf", // nginx config support
+
+				// Linux
+				"coolbear.systemd-unit-file",      // systemd unit file support
+				"nico-castell.linux-desktop-file", // desktop entry file support
 
 				// Tauri
-				"tauri-apps.tauri-vscode",
+				"tauri-apps.tauri-vscode", // tauri support
 
 				// Terraform / OpenTofu
-				"gamunu.opentofu",
+				"gamunu.opentofu", // OpenTofu support
 			}
 
 			// cSpell:enable
