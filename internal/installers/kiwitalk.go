320bd8e42d92e4adea7c05649e63493433452d96
developomp <developomp@protonmail.com>
2024-06-15 22:16:29 +0900

    refactor: replace kiwitalk appimage with nix package

--- internal/installers/kiwitalk.go
+++ internal/installers/kiwitalk.go
@@ -9,17 +9,7 @@ func init() {
 		Name: "KiwiTalk",
 		Desc: "KakaoTalk on Linux",
 		Setup: func() {
-			if wrapper.IsAppImageInstalled("kiwitalk") {
-				return
-			}
-
-			if !wrapper.IsFileInDir(wrapper.InHome("Downloads"), "kiwitalk") {
-				wrapper.DownloadFromGitHub("KiwiTalk", "KiwiTalk", "kiwi-talk_.*AppImage")
-				return
-			}
-		},
-		Reminders: []string{
-			"Install KiwiTalk via gearlever",
+			wrapper.NixOnce("nixpkgs#kiwitalk")
 		},
 	})
 }
